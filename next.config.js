16cc79a999eb0b111bc44424de976469d087f1fe
Leo Taube <10.leo.taube@gmail.com>
2024-01-21 18:13:54 +0100

    final changes

--- next.config.js
+++ next.config.js
@@ -1,16 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: "http",
-        hostname: "localhost",
-      },
-      {
-        protocol: "https",
-        hostname: "sellplatform-production.up.railway.app",
-      },
-    ],
+    domains: ["localhost", "sellplatform-production.up.railway.app"],
   },
 };
 
