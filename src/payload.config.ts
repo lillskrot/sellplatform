6e463c2ecdf343745ac1464c4809618fcb976316
Leo Taube <10.leo.taube@gmail.com>
2024-01-25 19:37:34 +0100

    added favicon

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -24,7 +24,7 @@ export default buildConfig({
     user: "users",
     bundler: webpackBundler(),
     meta: {
-      titleSuffix: "- DigitalHippo",
+      titleSuffix: "- TUBEN",
       favicon: "/favicon.ico",
       ogImage: "/thumbnail.jpg",
     },
